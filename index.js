bc00bd7f05e07505cf4d9df218f30f88a0d1fed1
elysian12 <elysian1232@gmail.com>
2022-04-08 22:02:43 +0530

    minor change

--- index.js
+++ index.js
@@ -22,6 +22,9 @@ app.use(express.json());
 app.use('/api/v1', authRoute);
 app.use('/api/v1/users', userRoute);
 
-app.listen(process.env.PORT || 3000, () => {
-    console.log(`listening on port ${process.env.PORT}`);
+app.listen(process.env.PORT || 3000, (error) => {
+    if (error) {
+        return console.error(err);
+    }
+    return console.log(`listening on port ${process.env.PORT || 3000}`);
 });
